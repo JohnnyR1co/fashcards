e7cb1aa1baffac148e8f3e2c936d07d84cb586a9
Johnny R1co <rubydev@bk.ru>
2015-09-23 23:06:30 +1000

    validations

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,4 @@
-class CardsController < ApplicationController  
-  
+class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
@@ -9,31 +8,44 @@ class CardsController < ApplicationController
   end
 
   def new
-    @card = Card.new  
+    @card = Card.new    
+  end
+
+  def edit
+    @card = Card.find(params[:id])   
   end
 
   def create
     @card = Card.new(card_params) 
+    @card[:original_text].capitalize!
+    @card[:translated_text].capitalize!
 
-    if @card.save
-      redirect_to @card, :notice => "The card is saved"
+    if @card.save 
+      redirect_to @card, notice: "The card is saved"     
     else
       render "new"
     end
   end
 
-  def edit   
+  def update
+    @card = Card.find(params[:id]) 
+
+    if @card.update(card_params)
+      redirect_to cards_path, notice: "The card has been updated"
+    else
+      render "edit"
+    end 
   end
 
-  def update   
-  end
-
-  def destroy   
+  def destroy 
+    @card = Card.find(params[:id])
+    @card.destroy
+ 
+    redirect_to cards_path, notice: "The card has been deleted"  
   end
 
   private
     def card_params
       params.require(:card).permit(:original_text, :translated_text, :review_date)
-    end
- 
+    end 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,12 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: {message: "Can't be blank"}
+  validates :original_text, uniqueness: { case_sensitive: false }
+  validate :translate
+
+  def translate
+    if original_text == translated_text
+      errors.add(:translated_text, "can't be the same")
+    end
+  end
+
 end
