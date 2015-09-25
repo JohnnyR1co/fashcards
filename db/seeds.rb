abd59b026d49dee38af3b5b778173a11ca4b5122
Johnny R1co <rubydev@bk.ru>
2015-09-25 20:17:57 +1000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "nokogiri"
+require "open-uri"
+
+doc = Nokogiri::HTML(open("http://www.papahuhu.com/vocabulary/itwords.html"))
+  
+doc.css("tr").each do |tr|
+  Card.create(original_text: tr.css("td:nth-child(1)").text,
+            translated_text: tr.css("td:nth-child(3)").text,
+            review_date: (DateTime.now.to_date + 3))
+  end
