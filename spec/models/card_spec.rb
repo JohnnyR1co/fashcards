50fedddd45cfc19233ecf9862eb3ccbe75be2982
Johnny R1co <rubydev@bk.ru>
2015-10-03 22:06:24 +1000

    edit card test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe Card, type: :model do
   describe "check" do
     it "check original and translated text" do
-      card = Card.new(original_text: "Apple", translated_text: "яблоко")
-      expect(card.translate).to be nil
+      card = Card.new(original_text: "Apple", translated_text: "Apple")
+      expect(card.translate).to eq(["can't be the same"])
     end
   end
 end
