dda5e4107ae3180945b3f7a4da8b7019809c311b
Johnny R1co <rubydev@bk.ru>
2015-10-10 20:10:26 +1000

    fix bugs

--- spec/features/check_spec.rb
+++ spec/features/check_spec.rb
@@ -2,13 +2,12 @@ require "rails_helper"
 
 describe "Checking card" do
 
-  let(:user) { create(:user) }
   let(:card) { create(:card) }
 
   it "redirects card to random when your translate is true" do
-    visit random_path(card)     
+    visit random_path(card)    
     fill_in "your_translate", with: "Тест"
     click_button "Check"
-    expect(page).to have_content "true"
+    expect(page).to have_content "Card checking"
   end
 end
