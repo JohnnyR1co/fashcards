2007a029095c11de2de51232f8cf4f59b6c38ce8
Johnny R1co <rubydev@bk.ru>
2015-10-11 13:41:24 +1000

    fix bugs

--- spec/features/check_spec.rb
+++ spec/features/check_spec.rb
@@ -5,7 +5,6 @@ describe "Checking card" do
 
   it "redirects card to random when your translate is true" do
     visit random_path(card)
-    save_and_open_page
     fill_in "your_translate", with: "Тест"
     click_button "Check"
     expect(page).to have_content "Card checking"
