54bed6a1b0ba07b6c2193d853e00ec746cd32cf4
Johnny R1co <rubydev@bk.ru>
2015-10-14 01:38:00 +1000

    add user_session controller

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,18 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:username], params[:password], params[:remember]) 
+      redirect_back_or_to root_url, :notice => "Login successful!"
+    else
+      flash.now[:alert] = "Login failed"
+      render "new"
+  end
+
+  def destroy
+    logout
+    redirect_to root_url, notice: "Logged out!"
+  end
+end
