081a6d92f5ece13362031d34e3a7ee74786e4e61
Johnny R1co <rubydev@bk.ru>
2015-10-18 00:54:38 +1000

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   patch "check" => "cards#check"
   get "login" => "user_sessions#new", as: :login
   post "logout" => "user_sessions#destroy", as: :logout
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback"
+  get "oauth/:provider" => "oauths#oauth", as: :auth_at_provider
 end
