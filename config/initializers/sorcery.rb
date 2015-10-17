21e2b8503e102a0d081911a64f9fde438cdc4d9f
Johnny R1co <rubydev@bk.ru>
2015-10-18 02:28:28 +1000

    fix bugs

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -1,10 +1,10 @@
-Rails.application.config.sorcery.submodules = [:remember_me, :external]
+Rails.application.config.sorcery.submodules = [:external, :remember_me]
 
 Rails.application.config.sorcery.configure do |config|
 
   config.external_providers = [:github]
-  config.github.key = "57c78c362773a51eb5ed"
-  config.github.secret = "6b33930089255f28b21c6c4ee8de585d6799759e"
+  config.github.key = "5dc5ba33b9dc14101227"
+  config.github.secret = "99115f4606913c1f58e808621e6a5ade29cbd3c4"
   config.github.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=github"
   config.github.user_info_mapping = {:email => "name"}
 
