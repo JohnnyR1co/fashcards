ff9a5913b888a5a13888bd75797f276935755013
Johnny R1co <rubydev@bk.ru>
2015-10-19 23:15:28 +1000

    add card_picture column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017142548) do
+ActiveRecord::Schema.define(version: 20151019131042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151017142548) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "user_id"
+    t.string   "card_picture"
   end
 
   add_index "cards", ["user_id"], name: "index_cards_on_user_id", using: :btree
