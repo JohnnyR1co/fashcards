a8ea6271d3155028d1956d2630837429b2f64f4c
Johnny R1co <rubydev@bk.ru>
2015-10-30 02:38:48 +1000

    fix bugs

--- app/models/card.rb
+++ app/models/card.rb
@@ -13,23 +13,23 @@ class Card < ActiveRecord::Base
   end
 
   def date_up
-      case self.check_count
-      when 0 then
-        self.check_count += 1
-        self.review_date += 12.hours
-      when 1 then
-        self.check_count += 1
-        self.review_date += 3.days
-      when 2 then
-        self.check_count += 1
-        self.review_date += 7.days
-      when 3 then
-        self.check_count += 1
-        self.review_date += 14.days
-      when 4 then
-        self.review_date += 1.month
-      end
-    self.save!
+    case self.check_count
+    when 0 then
+      self.check_count += 1  
+      self.review_date += 12.hours
+    when 1 then
+      self.check_count += 1
+      self.review_date += 3.days
+    when 2 then
+      self.check_count += 1
+      self.review_date += 7.days
+    when 3 then
+      self.check_count += 1
+      self.review_date += 14.days
+    when 4 then
+      self.review_date += 1.month
+    end
+   self.save!
   end
 
   def date_down
