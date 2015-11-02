ce84ca34d816c99e517103e7114219652f05ef2d
Johnny R1co <rubydev@bk.ru>
2015-11-03 02:02:53 +1000

    add mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver_now
       auto_login(@user)
       redirect_to root_url, notice: "Signed up!"
     else
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.mailgun.org",
+    :port                 => 587,
+    :user_name            => "postmaster@sandbox6915f90dd89f4d80a22905f79669452e.mailgun.org",
+    :password             => "5dc09bc16e5bde50419375cfe3b97c8e",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
