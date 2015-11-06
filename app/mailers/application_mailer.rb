2c79ed35ff4822089ffb92ca723fe9df0da34e9a
Johnny R1co <rubydev@bk.ru>
2015-11-06 22:58:10 +1000

    edit production.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "postmaster@sandbox6915f90dd89f4d80a22905f79669452e.mailgun.orgs"
+  default from: "rubydev@qq.com"
   layout "mailer"
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ApplicationMailer
-  default from: "postmaster@sandbox6915f90dd89f4d80a22905f79669452e.mailgun.org"
+  default from: "rubydev@qq.com"
 
   def welcome_email(user)
     @user = user
