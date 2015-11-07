d0ccffc58b4d7c71e06b73e58ffb17f483b77c2f
Johnny R1co <rubydev@bk.ru>
2015-11-07 15:26:38 +1000

    edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV["MAILGUN_API_KEY"],
+    api_host: ENV["MAILGUN_DOMAIN"]
 end
