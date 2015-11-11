72e745d362846b26dd35ae3e13f96989e2ffae2e
Johnny R1co <rubydev@bk.ru>
2015-11-11 22:56:56 +1000

    edit locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-     if current_user
+    if current_user
       I18n.locale = current_user.try(:locale)
     elsif
       I18n.locale = params[:locale]
